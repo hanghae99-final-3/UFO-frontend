98a07efc122979a5d11c19273f43dec7768201dd
che-ri <rnjs1135@gmail.com>
2021-09-14 18:31:57 +0900

    [fix] 초기 홈페이지에 진입시 result가 없는 오류 고침

--- final-project/src/Redux/Modules/freeBoardSlice.js
+++ final-project/src/Redux/Modules/freeBoardSlice.js
@@ -78,7 +78,7 @@ const freeBoardSlice = createSlice({
         //----자유게시판 목록 불러오는 리듀서
         [getFreeListDB.fulfilled]: (state, { payload }) => {
             state.list = payload?.result.rows;
-            state.pageCount = payload.result.countPage;
+            state.pageCount = payload?.result.countPage;
             state.isFetching = false;
             state.errorMessage = null;
         },
